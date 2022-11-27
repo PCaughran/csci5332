b659f23b1260ce85f4b7080bff5d591fe8e8fe39
PCaughran <81660207+PCaughran@users.noreply.github.com>
2022-11-26 21:47:33 -0500

    changes to the server class
    Changes primarily focus on the addition of over-internet functionality.

--- Server.java
+++ Server.java
@@ -1,26 +1,38 @@
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.InputMismatchException;
 import java.util.Scanner;
-
+import java.net.InetAddress;
 public class Server {
 	private ServerSocket serverSocket;
 	private String password;
+	static int portNumber=3050;
 	private Admin admin;
-	
-	//default constructor, will use default number of attempts provided
 	public Server(ServerSocket serverSocket) {
-		this(serverSocket, 3);
-	}
-	
-	//constructor where you can choose the number of attempts
-	public Server(ServerSocket serverSocket, int attempts) {
 		this.serverSocket = serverSocket;
 		this.password = establishPassword();
-		this.admin = new Admin(this.password, attempts);
+		this.admin = new Admin(this.password);
+
+	
 	}
 	
 	public void startServer() {
-		System.out.println("Secure Server Started");
+	
+		InetAddress SERVER_IP = null;
+		while(true)
+			try(Scanner scan = new Scanner(System.in)){
+			
+			portNumber = 3050;
+			SERVER_IP=InetAddress.getLocalHost(); 
+			if(SERVER_IP != null) break;
+			}catch(InputMismatchException ime) {
+				System.out.print("\nInput not of desired type, try again.");
+				continue;
+			}catch(Exception ex) {
+			ex.printStackTrace();
+			}
+		
+		System.out.printf("Secure Server Started on %s port# %d\n", SERVER_IP, portNumber);
 
 		try {
 			while(!serverSocket.isClosed()) {
@@ -69,7 +81,7 @@ public class Server {
 	
 	public static void main(String[] args) throws Exception {
 		
-		ServerSocket serverSocket = new ServerSocket(5332);
+		ServerSocket serverSocket = new ServerSocket(portNumber);
 		Server server = new Server(serverSocket);
 		server.startServer();
 		
